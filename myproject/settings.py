29526b7a16d2c8f7f491c408d69064fe6f0412d8
Aparna <writetoaparnans@gmail.com>
2026-07-16 09:31:17 +0530

    procfile added

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3sf0-%@u5slowh1vssk-%9gxqu4*od@i#r68s_4xr5w_!+yxoe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
